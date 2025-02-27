9b06e33d9524abdddcc142dabb6066e024f4f518
Andres Rivas <andres.rivas@utp.edu.co>
2025-02-27 12:47:15 -0500

    First Upload

---
+++ .web/pages/index.js
@@ -0,0 +1,194 @@
+/** @jsxImportSource @emotion/react */
+
+
+import { Fragment } from "react"
+import { Box as RadixThemesBox, Button as RadixThemesButton, Flex as RadixThemesFlex, Heading as RadixThemesHeading, Link as RadixThemesLink, Text as RadixThemesText } from "@radix-ui/themes"
+import NextLink from "next/link"
+import { isTrue } from "$/utils/state"
+import NextHead from "next/head"
+
+
+
+export function Link_072edd0fe5ab9622bea0d92a73560fc3 () {
+  
+
+
+
+
+
+  
+  return (
+    <RadixThemesLink asChild={true} css={({ ["&:hover"] : ({ ["color"] : "var(--accent-8)" }) })} target={(true ? "_blank" : "")}>
+
+<NextLink href={"https://www.ebay.com/itm/404563623981?_skw=superbike+toy&itmmeta=01JN2SW5VC5BRWH6APSS0P1EFP&hash=item5e31df002d:g:oosAAOSwDixjmvPs&itmprp=enc%3AAQAKAAAA8FkggFvd1GGDu0w3yXCmi1e8YgF%2BZOnjRi0Hp3I3xjXtpJ92Rc0RUyr0ZYOLYhEFsktt%2FfYnAt%2Bhs3Dl%2F0VJswZPTjGewVGBVveuiIGxI53zdUR%2BEsQOpgZznU2HpFe1ikH6CGTiFes%2BcfKQuAuRXBGOzrMFr80SFDG6buB1FLX3Scvc%2FAiWUToqhW6H5yDElGU7o5lkqqtIEUTYr3ZJQHSkpjMT7ETpUbJ3g0D2KVx8ZoF7chAYG1GW816POuip5SHAHW22AThM3X%2B1CqfctscT5RYiEdr6djKGjlCNlgUDJX9xWC5yclcycPDjaQ4bfQ%3D%3D%7Ctkp%3ABk9SR-7d8NmoZQ"} passHref={true}>
+
+<RadixThemesBox css={({ ["padding"] : "5px" })}>
+
+<img css={({ ["width"] : "260px", ["height"] : "200px" })} src={"/product_3.jpg"}/>
+</RadixThemesBox>
+</NextLink>
+</RadixThemesLink>
+  )
+}
+
+export function Link_d169aa0a80035501516e859b0a9bd578 () {
+  
+
+
+
+
+
+  
+  return (
+    <RadixThemesLink asChild={true} css={({ ["&:hover"] : ({ ["color"] : "var(--accent-8)" }) })} target={(true ? "_blank" : "")}>
+
+<NextLink href={"https://www.ebay.com/itm/387136422467?_trkparms=amclksrc%3DITM%26aid%3D1110018%26algo%3DHOMESPLICE.COMPLISTINGS%26ao%3D1%26asc%3D20220808120223%26meid%3Dab8e4cfa9bbd40d286677ac165cbbdaf%26pid%3D101546%26rk%3D15%26rkt%3D25%26sd%3D387136512271%26itm%3D387136422467%26pmt%3D1%26noa%3D0%26pg%3D3959035%26algv%3DPromotedCompV5WithHighAdFeeWithKnnRecallV1PostRankerMode1WithMyFG%26brand%3DMaisto&_trksid=p3959035.c101546.m1851&itmprp=cksum%3A387136422467ab8e4cfa9bbd40d286677ac165cbbdaf%7Cenc%3AAQAKAAABMHLP2FxpoV60wo2FCLo6WhwQgcx8Hy69mmKyqhd4ZFkoMvAyDIDbFBW64IYHO1rB%252FTm%252BcHC7Yz8svLFZRRyjiUc%252FYijA%252FNKFbGt87quOqeVBvRPLLDQQ7eFrRQ0eiqp1txrKlNNJlNLehw1Uzqu%252FdsP74ocFd12Z5koISdG0wYNN0EcjFudvPbfZrj247DVcpBi3sti4SlKRjuBlJEG62dZgxECUW%252FD2kRL2Z5IKY2y0ml5viy%252FCqpYjBB4A13Jv%252FIxRskXY%252BY%252B0JVYrgekh43KunhS77CNKYwhjMyfuzbIMm8gDQwr8xs0YnsFdK6EfeNT97y%252BNO9LMU%252BXxDuMbk3if4cyUKnQFKALFSK%252BSO4ILljX9Ha2uAPbNEKhW52bbMQRf7xFsatSA%252BphBRflGjJk%253D%7Campid%3APL_CLK%7Cclp%3A3959035&itmmeta=01JN42ATFDK2D49DD7BFVVJ98W"} passHref={true}>
+
+<RadixThemesBox css={({ ["padding"] : "5px" })}>
+
+<img css={({ ["width"] : "300px", ["height"] : "200px" })} src={"/product_1.jpg"}/>
+</RadixThemesBox>
+</NextLink>
+</RadixThemesLink>
+  )
+}
+
+export function Link_022de211faa885ec70f498088c6534c9 () {
+  
+
+
+
+
+
+  
+  return (
+    <RadixThemesLink asChild={true} css={({ ["&:hover"] : ({ ["color"] : "var(--accent-8)" }) })} target={(true ? "_blank" : "")}>
+
+<NextLink href={"https://www.facebook.com/profile.php?id=100069765637816/"} passHref={true}>
+
+<RadixThemesButton>
+
+{"\ud83d\udd25Hot Sales!"}
+</RadixThemesButton>
+</NextLink>
+</RadixThemesLink>
+  )
+}
+
+export function Link_bb2d3a96bb634cea59f923a18f86bb62 () {
+  
+
+
+
+
+
+  
+  return (
+    <RadixThemesLink asChild={true} css={({ ["&:hover"] : ({ ["color"] : "var(--accent-8)" }) })} target={(true ? "_blank" : "")}>
+
+<NextLink href={"https://www.ebay.com/itm/404633764752?_skw=superbike+toy&itmmeta=01JN2SW5VCWWQJN3Z4RNTV94ZR&hash=item5e360d4390:g:3esAAOSwI4pf~8un&itmprp=enc%3AAQAKAAAA8FkggFvd1GGDu0w3yXCmi1e53F%2FAfl2VeE3nJgRaQG9v1ZxbrP81nJUJoqIypjXOdntiF%2B5dFpkLQppBN%2FkQQljIZJt%2FCdQ3%2FS7QWnt5NQ91uCG2MhCYIRoiXQxZzIzeXL%2FroZhOExdVYfdgzkbB1OuvTjZov9eLZxqoREGRC5QNBAA2DKH93u922wouFcLCTKzZ46SSFj4BYO0Yp%2B5zJVRLJdw1tOKsUhlNBJ2yJIlV3F4qfHvcV%2B2u0mUmtNxAYAC0VG6TjevgJkaRDpV3hpqbUjOPn7r71AsnLTuVXiEwEkawHgXHXvtMyolepINFhA%3D%3D%7Ctkp%3ABk9SR-7d8NmoZQ"} passHref={true}>
+
+<RadixThemesBox css={({ ["padding"] : "5px" })}>
+
+<img css={({ ["width"] : "300px", ["height"] : "200px" })} src={"/product_2.jpg"}/>
+</RadixThemesBox>
+</NextLink>
+</RadixThemesLink>
+  )
+}
+
+export default function Component() {
+    
+
+
+
+
+  return (
+    <Fragment>
+
+<RadixThemesBox css={({ ["width"] : "100%", ["minHeight"] : "100vh", ["margin"] : "0", ["padding"] : "0", ["backgroundImage"] : "url('/Ducati_Background.jpg')", ["backgroundSize"] : "cover", ["backgroundPosition"] : "center center", ["backgroundRepeat"] : "no-repeat", ["backgroundAttachment"] : "fixed", ["overflowX"] : "hidden" })}>
+
+<RadixThemesFlex align={"start"} className={"rx-Stack"} css={({ ["background"] : "rgba(0, 0, 0, 0.2)", ["position"] : "sticky", ["top"] : "0", ["width"] : "100%", ["height"] : "4rem", ["zIndex"] : "10" })} direction={"row"} gap={"3"}>
+
+<RadixThemesBox css={({ ["position"] : "absolute", ["left"] : "3%", ["top"] : "50%", ["transform"] : "translateY(-50%)" })}>
+
+<RadixThemesHeading css={({ ["color"] : "black" })} size={"6"}>
+
+{"\ud83c\udfc1 SpeedFreaks"}
+</RadixThemesHeading>
+</RadixThemesBox>
+<RadixThemesBox css={({ ["position"] : "absolute", ["right"] : "5%", ["top"] : "50%", ["transform"] : "translateY(-50%)", ["display"] : "flex", ["gap"] : "2rem" })}>
+
+<RadixThemesLink asChild={true} css={({ ["color"] : "white", ["&:hover"] : ({ ["color"] : "var(--accent-8)" }) })}>
+
+<NextLink href={"#"} passHref={true}>
+
+{"Home"}
+</NextLink>
+</RadixThemesLink>
+<RadixThemesLink asChild={true} css={({ ["color"] : "white", ["&:hover"] : ({ ["color"] : "var(--accent-8)" }) })}>
+
+<NextLink href={"#"} passHref={true}>
+
+{"Products"}
+</NextLink>
+</RadixThemesLink>
+<RadixThemesLink asChild={true} css={({ ["color"] : "white", ["&:hover"] : ({ ["color"] : "var(--accent-8)" }) })}>
+
+<NextLink href={"#"} passHref={true}>
+
+{"Contact"}
+</NextLink>
+</RadixThemesLink>
+</RadixThemesBox>
+</RadixThemesFlex>
+<RadixThemesBox css={({ ["width"] : "100%" })}>
+
+<RadixThemesBox css={({ ["minHeight"] : "85vh", ["width"] : "100%", ["position"] : "relative" })}>
+
+<RadixThemesFlex align={"start"} className={"rx-Stack"} css={({ ["position"] : "absolute", ["right"] : "52%", ["top"] : "40%", ["transform"] : "translateY(-50%)", ["background"] : "rgba(0, 0, 0, 0.1)", ["padding"] : "2rem", ["borderRadius"] : "10px", ["alignItems"] : "flex-start" })} direction={"column"} gap={"5"}>
+
+<RadixThemesHeading css={({ ["color"] : "black" })} size={"9"}>
+
+{"\ud83c\udfc1 SpeedFreaks"}
+</RadixThemesHeading>
+<RadixThemesText as={"p"} css={({ ["color"] : "wihte" })} size={"5"}>
+
+{"Crafted for Enthusiasts, Driven by Speed"}
+</RadixThemesText>
+<Link_022de211faa885ec70f498088c6534c9/>
+</RadixThemesFlex>
+</RadixThemesBox>
+<RadixThemesFlex align={"start"} className={"rx-Stack"} css={({ ["paddingTop"] : "2rem", ["paddingBottom"] : "2rem", ["width"] : "100%", ["alignItems"] : "center" })} direction={"column"} gap={"5"}>
+
+<RadixThemesHeading css={({ ["color"] : "white" })} size={"7"}>
+
+{"Limited Edition"}
+</RadixThemesHeading>
+<RadixThemesFlex css={({ ["alignItems"] : "center", ["width"] : "100%", ["flexWrap"] : "wrap", ["gap"] : "10px" })} justify={"center"}>
+
+<Link_d169aa0a80035501516e859b0a9bd578/>
+<Link_bb2d3a96bb634cea59f923a18f86bb62/>
+<Link_072edd0fe5ab9622bea0d92a73560fc3/>
+</RadixThemesFlex>
+</RadixThemesFlex>
+</RadixThemesBox>
+<RadixThemesBox css={({ ["background"] : "rgba(0, 0, 0, 0.6)", ["textAlign"] : "center", ["width"] : "100%", ["padding"] : "2rem" })}>
+
+<RadixThemesText as={"p"} css={({ ["color"] : "white" })}>
+
+{"\u00a9 2025 SpeedFreaks. All rights reserved."}
+</RadixThemesText>
+</RadixThemesBox>
+</RadixThemesBox>
+<NextHead>
+
+<title>
+
+{"ArWebsite | Index"}
+</title>
+<meta content={"favicon.ico"} property={"og:image"}/>
+</NextHead>
+</Fragment>
+  )
+}
