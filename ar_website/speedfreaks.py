b6100d44124d65f051107e4b8b99e2e51f41aa2a
Andres Rivas <andres.rivas@utp.edu.co>
2025-02-28 21:41:18 -0500

    Update speedfreaks.py

--- ar_website/speedfreaks.py
+++ ar_website/speedfreaks.py
@@ -139,7 +139,7 @@ def hero_section() -> rx.Component:
             ),
             rx.link(
                 rx.button("🔥Hot Sales!"),
-                href="https://www.facebook.com/profile.php?id=100069765637816%2F",
+                href="https://www.facebook.com/share/1UeemmGbZq/?mibextid=wwXIfr",
                 is_external=True,
             ),
             spacing="5",
